d638e4207112aa5beb692505a80cebcca4371d02
TabbyToffee <147265710+TabbyToffee@users.noreply.github.com>
2025-10-27 16:52:27 +0000

    Create new spd2010 crate
    All previous history is in the main repository
    This crate currently only contains the touch driver, not display

---
+++ src/touch/io.rs
@@ -0,0 +1,45 @@
+use super::{Error, SPD2010_ADDR, SPD2010Touch};
+use crate::touch::InterruptInput;
+use embedded_hal::{
+    delay::DelayNs,
+    i2c::{I2c, Operation},
+};
+
+impl<'a, I2C: I2c, Ti: InterruptInput> SPD2010Touch<'a, I2C, Ti> {
+    pub fn read_register(&mut self, reg: u16, data: &mut [u8]) -> Result<(), Error<I2C>> {
+        let reg_bytes = [reg as u8, (reg >> 8) as u8];
+        self.i2c
+            .write(SPD2010_ADDR, &reg_bytes)
+            .map_err(Error::I2C)?;
+        self.i2c.read(SPD2010_ADDR, data).map_err(Error::I2C)?;
+        Ok(())
+    }
+
+    pub fn write_command(&mut self, reg: u16, data: &[u8]) -> Result<(), Error<I2C>> {
+        let reg_bytes = [reg as u8, (reg >> 8) as u8];
+
+        self.i2c
+            .transaction(
+                SPD2010_ADDR,
+                &mut [Operation::Write(&reg_bytes), Operation::Write(data)],
+            )
+            .map_err(Error::I2C)?;
+
+        Ok(())
+    }
+
+    pub async fn clear_interrupt<DELAY: DelayNs>(
+        &mut self,
+        delay: &mut DELAY,
+    ) -> Result<(), Error<I2C>> {
+        let ack: [u8; 2] = [0x01, 0x00]; // step 1: ACK (acknowledge interrupt)
+        let rearm: [u8; 2] = [0x00, 0x00]; // step 2: re-arm (setup interrupt again)
+
+        self.write_command(0x0002, &ack)?; // ack
+        delay.delay_us(200);
+        self.write_command(0x0002, &rearm)?; // re-arm
+        delay.delay_ms(10);
+
+        Ok(())
+    }
+}
---
+++ src/touch/mod.rs
@@ -0,0 +1,120 @@
+pub mod driver;
+pub mod io;
+
+pub use driver::reset;
+
+use core::cell::RefCell;
+
+use critical_section::Mutex;
+use embedded_hal::i2c::I2c;
+use heapless::Vec;
+
+const SPD2010_ADDR: u8 = 0x53;
+const SPD2010_MAX_TOUCH_POINTS: usize = 10;
+
+#[derive(Debug)]
+pub enum Error<I2C: I2c> {
+    I2C(I2C::Error),
+    InterruptStayedHigh,
+}
+
+// All touches and gesture info
+#[derive(Default, Debug)]
+pub struct TouchData {
+    pub points: Vec<TouchPoint, SPD2010_MAX_TOUCH_POINTS>,
+    pub touch_count: u8,
+    pub gesture: u8,
+    pub down: bool,
+    pub up: bool,
+    pub down_x: u16,
+    pub down_y: u16,
+    pub up_x: u16,
+    pub up_y: u16,
+}
+
+// Single touch
+#[derive(Default, Debug)]
+pub struct TouchPoint {
+    pub id: u8,
+    pub x: u16,
+    pub y: u16,
+    pub weight: u8,
+}
+
+#[derive(Debug)]
+struct StatusLow {
+    pt_exist: bool,
+    gesture: bool,
+    key: bool,
+    aux: bool,
+    keep: bool,
+    raw_or_pt: bool,
+    none6: bool,
+    none7: bool,
+}
+
+#[derive(Debug)]
+struct StatusHigh {
+    none0: bool,
+    none1: bool,
+    none2: bool,
+    cpu_run: bool,
+    tint_low: bool,
+    tic_in_cpu: bool,
+    tic_in_bios: bool,
+    tic_busy: bool,
+}
+
+#[derive(Debug)]
+struct TouchStatus {
+    status_low: StatusLow,
+    status_high: StatusHigh,
+    read_len: u16,
+}
+
+#[derive(Default)]
+struct HDPStatus {
+    status: u8,
+    next_packet_len: u16,
+}
+
+pub struct SPD2010Touch<'a, I2C: I2c, Ti: InterruptInput> {
+    i2c: I2C,
+    touch_interrupt: &'a Mutex<RefCell<Option<Ti>>>,
+}
+
+impl<'a, I2C: I2c, Ti: InterruptInput> SPD2010Touch<'a, I2C, Ti> {
+    pub fn new(i2c: I2C, touch_interrupt: &'a Mutex<RefCell<Option<Ti>>>) -> Self {
+        // touch_interrupt.listen(Event::FallingEdge);
+
+        Self {
+            i2c,
+            touch_interrupt,
+        }
+    }
+
+    fn clear_interrupt_flag(&self) {
+        critical_section::with(|cs| {
+            self.touch_interrupt
+                .borrow_ref_mut(cs)
+                .as_mut()
+                .unwrap()
+                .clear_interrupt();
+        });
+    }
+
+    fn get_interrupt_flag(&self) -> bool {
+        critical_section::with(|cs| {
+            self.touch_interrupt
+                .borrow_ref(cs)
+                .as_ref()
+                .unwrap()
+                .get_interrupt()
+        })
+    }
+}
+
+pub trait InterruptInput {
+    fn get_interrupt(&self) -> bool;
+    fn clear_interrupt(&mut self);
+}
